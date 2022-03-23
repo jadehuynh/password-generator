4af1827e484425714dcd2c7eb5baa4251a576e14
jadehuynh <jadehuynh08@gmail.com>
2022-03-23 17:10:28 -0500

    added arrays

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
+var digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
