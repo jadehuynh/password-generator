29f549eda04628fee6752e79e36ad87b834a92d6
jadehuynh <jadehuynh08@gmail.com>
2022-03-23 20:33:07 -0500

    functions and if else

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var upperCase =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
 var digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var newPassword = "";
 // Write password to the #password input
+//var pool = []
+
 var userSelect = {
-  upper: false,
-  lower: false,
-  num: false,
-  spec: false,
+  upper: undefined,
+  lower: undefined,
+  num: undefined,
+  spec: undefined,
 }
 
 function writePassword() {
@@ -25,41 +28,57 @@ function writePassword() {
 
 
 // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 function generatePassword() {
   var charInput= prompt("Please select between 8 and 128 characters.")
     if (charInput < 8 || charInput > 128) {
-      alert('Selection was incorrect. Please select between 8 and 128 characters.')
+      alert("Selection was incorrect. Please select between 8 and 128 characters.")
         generatePassword()
     }
 var userChoice= confirm("Would you like lowercase characters?")
     if (userChoice) {
       userSelect.lower= true
-      console.log(userChoice.lower)
+      console.log(userSelect.lower)
+    } else {
+      userSelect.lower=false
+      console.log(userSelect.lower);
     }
 var userChoice= confirm("Would you like uppercase characters?")
     if (userChoice) {
       userSelect.upper= true
-      console.log(userChoice.upper)
+      console.log(userSelect.upper)
+    }  else {
+      userSelect.upper=false
+      console.log(userSelect.upper);
     }
 var userChoice= confirm("Would you like numeric characters?")
     if (userChoice) {
-      userSelect.num= true
-      console.log(userChoice.num)
+      userSelect.digits= true
+      console.log(userSelect.digits)
+    } else {
+      userSelect.digits=false
+      console.log(userSelect.digits);
     }
 var userChoice= confirm("Would you like special characters?")
     if (userChoice) {
       userSelect.spec= true
-      console.log(userChoice.spec)
+      console.log(userSelect.spec)
+    } else {
+      userSelect.spec=false
+      console.log(userSelect.spec);
     }
 
-if (userSelect.lower === false && userSelect.upper === false && userSelect.num === false && userSelect.spec === false) {
+if (userSelect.lower === false && userSelect.upper === false && userSelect.digits === false && userSelect.spec === false) {
     alert("Please select at least one character type.")
     generatePassword()
   }
-  console.log(charInput)
-  return;
+  //return generatePassword()
 }
-
-
-
-generateBtn.addEventListener("click", writePassword);
+function passGen (x) {
+for (let i = 0; i < userSelect; i++) {
+  var selectedChar = userSelect[Math.floor(Math.random() * userSelect.length)]
+  newPassword = newPassword.concat(selectedChar[Math.floor(Math.random() * selectedChar.length) ])
+}
+}
